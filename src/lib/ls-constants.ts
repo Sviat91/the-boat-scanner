f063c318c3905d1cab67ae3525eabe4366e1d7e5
Sviat91 <167562324+Sviat91@users.noreply.github.com>
2025-07-02 21:07:56 +0200

    Update ls-constants.ts

--- src/lib/ls-constants.ts
+++ src/lib/ls-constants.ts
@@ -1,2 +1,2 @@
 export const LS_PACK3_URL = 'https://theboatscanner.lemonsqueezy.com/buy/8fbe2711-602c-4b20-83ee-80afce7b5e49?embed=1'
-export const LS_PACK12_URL = 'https://theboatscanner.lemonsqueezy.com/buy/b4788b8f-537d-49de-ab10-bea483161546?embed=1'
+export const LS_PACK12_URL = 'https://theboatscanner.lemonsqueezy.com/buy/b4788b8f-537d-49de-ab10-bea483161546'
