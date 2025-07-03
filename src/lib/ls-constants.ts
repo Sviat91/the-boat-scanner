bfdf53aa133e6af8869ce610a0d4358a9d59df19
Sviat91 <167562324+Sviat91@users.noreply.github.com>
2025-07-03 21:22:06 +0200

    Update ls-constants.ts

--- src/lib/ls-constants.ts
+++ src/lib/ls-constants.ts
@@ -1,8 +1,8 @@
 import { supabase } from '@/lib/supabase'
 
-export const LS_PACK3_URL = 'https://shop.theboatscanner.com/buy/78c6c711-133d-4cd3-ae18-fc66bde0c1b6'
-export const LS_PACK12_URL = 'https://shop.theboatscanner.com/buy/64232b02-cd51-4151-99c2-d439a4f4cd53'
-export const LS_UNLIMITED_URL = 'https://shop.theboatscanner.com/buy/618d34de-5dbc-4f77-93ce-92b503b4b3d8'
+export const LS_PACK3_URL = 'https://shop.theboatscanner.com/buy/8fbe2711-602c-4b20-83ee-80afce7b5e49'
+export const LS_PACK12_URL = 'https://shop.theboatscanner.com/buy/b4788b8f-537d-49de-ab10-bea483161546'
+export const LS_UNLIMITED_URL = 'https://shop.theboatscanner.com/buy/dd6c1601-60e0-4f8d-857f-7aca6138d8fc'
 
 export const buildLsUrl = async (kind: 'pack3' | 'pack12' | 'unlimited'): Promise<string> => {
   const baseUrl =
