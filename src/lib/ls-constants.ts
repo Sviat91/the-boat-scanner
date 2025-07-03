02403e62ad3f7b06f101eda486a4e710dd0f9513
Sviat91 <167562324+Sviat91@users.noreply.github.com>
2025-07-03 13:57:33 +0200

    Update ls-constants.ts

--- src/lib/ls-constants.ts
+++ src/lib/ls-constants.ts
@@ -1,2 +1,2 @@
 export const LS_PACK3_URL = 'https://shop.theboatscanner.com/buy/8fbe2711-602c-4b20-83ee-80afce7b5e49'
-export const LS_PACK12_URL = '<a href="https://shop.theboatscanner.com/buy/b4788b8f-537d-49de-ab10-bea483161546?embed=1" class="lemonsqueezy-button">Buy 12 search credits</a><script src="https://assets.lemonsqueezy.com/lemon.js" defer></script>'
+export const LS_PACK12_URL = 'https://shop.theboatscanner.com/buy/b4788b8f-537d-49de-ab10-bea483161546'
