bd1ed276c85f72c5ef362a4e1946c0aa9de4dafc
Sviat91 <167562324+Sviat91@users.noreply.github.com>
2025-07-08 13:43:08 +0200

    Run disableAutoSelect after GIS script loads; One-Tap spinner fixed

---
+++ src/utils/disableOneTap.ts
@@ -0,0 +1,8 @@
+const waitForGis = () => new Promise<void>(res => {
+  if (window.google?.accounts?.id) return res();
+  const i = setInterval(() => {
+    if (window.google?.accounts?.id) { clearInterval(i); res(); }
+  }, 50);
+});
+
+waitForGis().then(() => window.google.accounts.id.disableAutoSelect());
