c5698b54cb47f85689b9a5aca37efcd9ac83ea9b
Sviat91 <167562324+Sviat91@users.noreply.github.com>
2025-07-09 16:17:36 +0200

    Revert "Implement device-aware Google One Tap auth"

--- src/utils/device.ts
+++
@@ -1,6 +0,0 @@
-export function isMobileDevice() {
-  if (typeof navigator === 'undefined') return false;
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  );
-}
